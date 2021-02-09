92edac3ea55d944d8be8428ddd7189b087a4b5a2
Engin Karataş <enginkaratas99@gmail.com>
2021-02-09 08:55:36 +0300

    setup

---
+++ SqlWorkWithCsharp/SqlWorkWithCsharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SqlWorkWithCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
